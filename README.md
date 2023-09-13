5c4a7c357766639a9cbafbc03fe1e13a8697c54f
quasat529 <quasar0529@gmail.com>
2023-09-13 03:06:28 +0000

    arrange commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# LoRA for Easy Training and Lightweighting
-
-## Dataset
-
-https://github.com/neychev/small_DL_repo/tree/master/datasets/Multi30k
-https://github.com/neychev/small_DL_repo/tree/master/datasets/Multi30k
--- custom_template/data.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import pandas as pd
-
-urllib.request.urlretrieve(
-    "https://raw.githubusercontent.com/LawrenceDuan/IMDb-Review-Analysis/master/IMDb_Reviews.csv",
-    filename="IMDb_Reviews.csv",
-)
